bd7b1054ed8b529653e56c66672bef67f509da78
mahuke <mahuke777@gmail.com>
2023-06-27 22:53:45 +0300

    last commit

--- test git.cpp
+++ test git.cpp
@@ -3,16 +3,10 @@
 using namespace std;
 int main()
 {
-<<<<<<< HEAD
     std::cout << "Hello World!\n";
     // i dont know what to do in git now but very soon i will profesional programmer
     int i = 0;
     while (i) {
         i += 1;
-=======
-    cout << "Hello World!\n";
-    for (int i = 0; i < 10; i++) {
-        cout << i << " ";
->>>>>>> new
     }
 }
