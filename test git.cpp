1035bf82ce586e76f529365799056952f7ba4043
mahuke <mahuke777@gmail.com>
2023-06-27 21:43:57 +0300

    first commit in master

--- test git.cpp
+++ test git.cpp
@@ -4,4 +4,9 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    // i dont know what to do in git now but very soon i will profesional programmer
+    int i = 0;
+    while (i) {
+        i += 1;
+    }
 }
